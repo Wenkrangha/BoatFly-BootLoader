f031dca77d5bcc173863d9efad6283f22c1eb4e5
Wenkrang <2934356978@qq.com>
2024-04-13 21:23:20 +0800

    Initial commit

---
+++ src/main/java/com/wenkrang/boatflybootloader/BoatFly_BootLoader.java
@@ -0,0 +1,17 @@
+package com.wenkrang.boatflybootloader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BoatFly_BootLoader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
